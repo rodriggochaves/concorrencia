59c7d2b78cb659d17655aea58361c77f89a3d9d4
vitor pontes <vitormax2005@hotmail.com>
2016-05-16 19:47:16 -0300

    Inicialização e destruição de loja na main

--- src/code/main.c
+++ src/code/main.c
@@ -2,17 +2,24 @@
 #include "../include/pessoa.h"
 #include "../include/shop.h"
 #include "../include/loja.h"
+
 #include <stdio.h>
 
 int main(void){
   int i;
   cria_shop();
 
-  for( i= 0; i<PESSOAS;i++){
+  for(i = 0; i<PESSOAS;i++){
     criar_pessoa(i);
+  }
+  for (i = 0; i < LOJAS; ++i){
+    criar_loja(i);
   }
   for(i=0;i<PESSOAS;i++){
     destruir_pessoa(i);
+  }
+  for(i = 0;i<LOJAS;i++){
+    destruir_loja(i);
   }
   imprime_shop();
   return 0;  
